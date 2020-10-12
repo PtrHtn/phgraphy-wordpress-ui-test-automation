6c3a23dd7180188cb31735fde1d2f94a6244ae7a
PtrHtn <ptrhtn@gmail.com>
2020-10-12 19:11:10 -0300

    Add CucumberHooks.java

---
+++ src/test/java/CucumberHooks.java
@@ -0,0 +1,28 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CucumberHooks {
+    public static WebDriver driver;
+    public static WebDriverWait driverWait;
+
+    @Before
+    public void beforeScenario() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @After
+    public void afterScenario() {
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+
+}
