2829e14a72e6399b4a48ee40d42f676a09acc0ef
PtrHtn <ptrhtn@gmail.com>
2020-10-13 18:37:19 -0300

    Update PHgraphyCom step definitions file

---
+++ src/test/java/step_definitions/PHgraphyCom.java
@@ -0,0 +1,14 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class PHgraphyCom {
+    @Given("user is on the homepage of phgraphy.com website")
+    public void userIsOnTheHomepageOfPhgraphyComWebsite() {
+    }
+
+    @Then("header image is displayed on the top of page")
+    public void headerImageIsDisplayedOnTheTopOfPage() {
+    }
+}
