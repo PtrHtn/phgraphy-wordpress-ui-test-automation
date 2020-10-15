49ad0ec22ca41688bf614a9df90db9ead2354b9f
PtrHtn <ptrhtn@gmail.com>
2020-10-15 18:49:15 -0300

    Update User.java

--- src/test/java/page_objects/User.java
+++ src/test/java/page_objects/User.java
@@ -1,6 +1,8 @@
 package page_objects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import step_definitions.CucumberHooks;
 
@@ -9,4 +11,13 @@ public class User {
     private final WebDriver driver = step_definitions.CucumberHooks.driver;
     private final WebDriverWait driverWait = step_definitions.CucumberHooks.driverWait;
 
+    private final By headerImageLocator = By.id("header-image");
+
+
+    public User user() { return this; }
+
+    public void navigateTo(String url){
+        driver.get(url);
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(headerImageLocator));
+    }
 }
