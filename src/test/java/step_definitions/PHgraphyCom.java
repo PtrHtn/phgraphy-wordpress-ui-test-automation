f0342178cb53f89a7978ed6111b6ac73285d5b72
PtrHtn <ptrhtn@gmail.com>
2020-10-15 18:49:40 -0300

    Update PHgraphyCom.java

--- src/test/java/step_definitions/PHgraphyCom.java
+++ src/test/java/step_definitions/PHgraphyCom.java
@@ -2,10 +2,12 @@ package step_definitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import page_objects.User;
 
-public class PHgraphyCom {
+public class PHgraphyCom  extends User {
     @Given("user is on the homepage of phgraphy.com website")
     public void userIsOnTheHomepageOfPhgraphyComWebsite() {
+            user().navigateTo("https://phgraphy.com/");
     }
 
     @Then("header image is displayed on the top of page")
