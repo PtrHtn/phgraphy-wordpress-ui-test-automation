5251e50e1604b640e0bf5ac7076314f718e9b3f3
PtrHtn <ptrhtn@gmail.com>
2020-10-21 16:28:21 -0300

    Add canSeeCookiePolicyBar method

--- src/test/java/page_objects/User.java
+++ src/test/java/page_objects/User.java
@@ -13,6 +13,7 @@ public class User {
     private final WebDriverWait driverWait = step_definitions.CucumberHooks.driverWait;
 
     private final By headerImageLocator = By.id("header-image");
+    private final By cookiePolicyBarLocator = By.cssSelector("div[class='widget widget_eu_cookie_law_widget'] form");
 
 
     public User user() { return this; }
@@ -26,4 +27,10 @@ public class User {
         Assertions.assertTrue(driver.findElement(headerImageLocator).isEnabled(),
                 "\n\nUser can NOT see header image on the top of page!\n\n");
     }
+
+    public void canSeeCookiePolicyBar() {
+        Assertions.assertTrue(driver.findElement(cookiePolicyBarLocator).isDisplayed(),
+                "\n\nUser can NOT see Cookie Policy bar on the bottom of page!\n\n");
+    }
+
 }
